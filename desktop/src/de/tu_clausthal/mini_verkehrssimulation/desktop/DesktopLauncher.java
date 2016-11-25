ecc13f66d7e96fadc6aebb5533d24866cdaddd44
Ehsan Tatasadi <ehsan.tatasadi@gmail.com>
2016-11-25 12:23:03 +0100

    Create LibGDX Project

---
+++ desktop/src/de/tu_clausthal/mini_verkehrssimulation/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.tu_clausthal.mini_verkehrssimulation.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.tu_clausthal.mini_verkehrssimulation.Simulation;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Simulation(), config);
+	}
+}
