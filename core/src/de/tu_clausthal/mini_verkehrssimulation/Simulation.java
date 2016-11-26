428a913b0384413e20d53398c433433e982c6966
Ehsan Tatasadi <ehsan.tatasadi@gmail.com>
2016-11-26 13:30:20 +0100

    changing resulation, creating a traffic light
    there is now more blocks on each road.
    creating a traffic light with ShapeRenderer.

--- core/src/de/tu_clausthal/mini_verkehrssimulation/Simulation.java
+++ core/src/de/tu_clausthal/mini_verkehrssimulation/Simulation.java
@@ -5,11 +5,15 @@ package de.tu_clausthal.mini_verkehrssimulation;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
@@ -31,6 +35,7 @@ public class Simulation extends ApplicationAdapter{
 	SpriteBatch sb;
     Texture carTexture;
     Sprite carSprite;
+    ShapeRenderer trafficLightShapeRenderer;
 
 	@Override
 	public void create () {
@@ -49,10 +54,13 @@ public class Simulation extends ApplicationAdapter{
 		carSprite = new Sprite(carTexture);
 
 		// going left to right
-		carSprite.setPosition(-carSprite.getWidth(), Gdx.graphics.getHeight() / 2 - 48); 
+		carSprite.setPosition(-carSprite.getWidth(), Gdx.graphics.getHeight() / 2 - 24); 
 		// going up to down
 		//carSprite.setPosition(Gdx.graphics.getWidth()/2 - 64, Gdx.graphics.getHeight());
 		//carSprite.setRotation(-90);
+		
+		trafficLightShapeRenderer = new ShapeRenderer();
+		
 
 	}
 	
@@ -64,7 +72,7 @@ public class Simulation extends ApplicationAdapter{
 
 	@Override
 	public void render () {
-        carSprite.translateX(1f);
+        carSprite.translateX(1);
 
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
@@ -75,5 +83,12 @@ public class Simulation extends ApplicationAdapter{
 		sb.begin();
 		carSprite.draw(sb);
         sb.end();
+		
+		trafficLightShapeRenderer.begin(ShapeType.Filled);
+		trafficLightShapeRenderer.setColor(Color.RED);
+		//trafficLightShapeRenderer.setColor(Color.GREEN);
+		trafficLightShapeRenderer.circle(576, 560, 8);
+		trafficLightShapeRenderer.end();
+		
 	}
 }
