ccd90cce1f66060a99158217d11a99fd9b3e81ee
Ehsan Tatasadi <ehsan.tatasadi@gmail.com>
2016-11-26 10:50:09 +0100

    simple car movement
    without physics. with translation.

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 A crossroads simulation in Java
 
 using LibGDX tutorial series: http://www.gamefromscratch.com/page/LibGDX-Tutorial-series.aspx  
+
 Roads tile set: http://opengameart.org/content/road-tile-textures
+
 Car model: http://opengameart.org/content/red-car-top-down
--- core/src/de/tu_clausthal/mini_verkehrssimulation/Simulation.java
+++ core/src/de/tu_clausthal/mini_verkehrssimulation/Simulation.java
@@ -31,9 +31,7 @@ public class Simulation extends ApplicationAdapter{
 	SpriteBatch sb;
     Texture carTexture;
     Sprite carSprite;
-    World world;
-    Body body;
-	
+
 	@Override
 	public void create () {
 		float w = Gdx.graphics.getWidth();
@@ -42,47 +40,20 @@ public class Simulation extends ApplicationAdapter{
         camera = new OrthographicCamera();
         camera.setToOrtho(false,w,h);
         camera.update();
+        
 		roadsTiledMap = new TmxMapLoader().load("roads.tmx");
 		roadsTiledMapRenderer = new OrthogonalTiledMapRenderer(roadsTiledMap);
 		
 		sb = new SpriteBatch();
 		carTexture = new Texture(Gdx.files.internal("car.png"));
 		carSprite = new Sprite(carTexture);
-		// the car come from left
+
+		// going left to right
 		carSprite.setPosition(-carSprite.getWidth(), Gdx.graphics.getHeight() / 2 - 48); 
+		// going up to down
+		//carSprite.setPosition(Gdx.graphics.getWidth()/2 - 64, Gdx.graphics.getHeight());
+		//carSprite.setRotation(-90);
 
-        // Create a physics world, the heart of the simulation.  The Vector passed in is gravity
-        world = new World(new Vector2(98f, 0), true);
-
-        // Now create a BodyDefinition.  This defines the physics objects type and position in the simulation
-        BodyDef bodyDef = new BodyDef();
-        bodyDef.type = BodyDef.BodyType.DynamicBody;
-        // We are going to use 1 to 1 dimensions.  Meaning 1 in physics engine is 1 pixel
-        // Set our body to the same position as our sprite
-        bodyDef.position.set(carSprite.getX(), carSprite.getY());
-
-        // Create a body in the world using our definition
-        body = world.createBody(bodyDef);
-
-        // Now define the dimensions of the physics shape
-        PolygonShape shape = new PolygonShape();
-        // We are a box, so this makes sense, no?
-        // Basically set the physics polygon to a box with the same dimensions as our sprite
-        shape.setAsBox(carSprite.getWidth()/2, carSprite.getHeight()/2);
-
-        // FixtureDef is a confusing expression for physical properties
-        // Basically this is where you, in addition to defining the shape of the body
-        // you also define it's properties like density, restitution and others we will see shortly
-        // If you are wondering, density and area are used to calculate over all  mass
-        FixtureDef fixtureDef = new FixtureDef();
-        fixtureDef.shape = shape;
-        fixtureDef.density = 1f;
-
-        Fixture fixture = body.createFixture(fixtureDef);
-
-        // Shape is the only disposable of the lot, so get rid of it
-        shape.dispose();
-		
 	}
 	
 	@Override
@@ -93,23 +64,16 @@ public class Simulation extends ApplicationAdapter{
 
 	@Override
 	public void render () {
-		// Advance the world, by the amount of time that has elapsed since the last frame
-        // Generally in a real game, dont do this in the render loop, as you are tying the physics
-        // update rate to the frame rate, and vice versa
-        world.step(Gdx.graphics.getDeltaTime(), 6, 2);
-
-        // Now update the sprite position accordingly to it's now updated Physics body
-        carSprite.setPosition(body.getPosition().x, body.getPosition().y);
+        carSprite.translateX(1f);
 
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		//camera.update();
 		roadsTiledMapRenderer.setView(camera);
 		roadsTiledMapRenderer.render();
 		
 		sb.begin();
-		sb.draw(carSprite, carSprite.getX(), carSprite.getY());
+		carSprite.draw(sb);
         sb.end();
 	}
 }
