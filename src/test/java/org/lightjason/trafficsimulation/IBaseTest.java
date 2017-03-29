f1aae4e32833715cb1d34d7301b2ad65dc5ca037
Philipp Kraus <philipp.kraus@flashpixx.de>
2017-03-29 10:48:24 +0200

    add communication message to test

--- src/test/java/org/lightjason/trafficsimulation/IBaseTest.java
+++ src/test/java/org/lightjason/trafficsimulation/IBaseTest.java
@@ -1,20 +1,27 @@
 package org.lightjason.trafficsimulation;
 
+import org.junit.Assert;
+import org.lightjason.trafficsimulation.actions.CBroadcast;
+import org.lightjason.trafficsimulation.actions.CSend;
 import org.lightjason.trafficsimulation.simulation.EObjectFactory;
 import org.lightjason.trafficsimulation.simulation.IObject;
 import org.lightjason.trafficsimulation.simulation.algorithm.routing.ERoutingFactory;
 import org.lightjason.trafficsimulation.simulation.environment.IEnvironment;
 import org.junit.Assume;
 import org.junit.Before;
+import org.lightjason.agentspeak.common.CCommon;
 import org.lightjason.agentspeak.action.IAction;
 import org.lightjason.agentspeak.agent.IAgent;
 import org.lightjason.agentspeak.language.score.IAggregation;
 
 import java.io.FileInputStream;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.LogManager;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.Assert.assertTrue;
 
@@ -26,8 +33,15 @@ import static org.junit.Assert.assertTrue;
  */
 public abstract class IBaseTest
 {
+    private final Map<String, IAgent<?>> m_agents = new ConcurrentHashMap<>();
 
-    private static final Set<IAction> ACTIONS = org.lightjason.agentspeak.common.CCommon.actionsFromPackage().collect( Collectors.toSet() );
+    private final Set<IAction> m_actions = Stream.concat(
+                                                Stream.of(
+                                                    new CSend( m_agents ),
+                                                    new CBroadcast( m_agents )
+                                                ),
+                                                CCommon.actionsFromPackage()
+                                           ).collect( Collectors.toSet() );
 
     protected IEnvironment m_environment;
 
@@ -45,13 +59,16 @@ public abstract class IBaseTest
     @Before
     public final void initializeenvironment() throws Exception
     {
+        Assume.assumeNotNull( m_actions );
+        Assume.assumeNotNull( m_environment );
+
         try
         (
             final FileInputStream l_stream = new FileInputStream( "src/test/resources/environment.asl" );
         )
         {
             m_environment = EObjectFactory.ENVIRONMENT
-                .generate( l_stream, ACTIONS.stream(), IAggregation.EMPTY )
+                .generate( l_stream, m_actions.stream(), IAggregation.EMPTY )
                 .generatesingle( 100, 100, 25, ERoutingFactory.JPSPLUS.get() )
                 .raw();
         }
@@ -73,6 +90,7 @@ public abstract class IBaseTest
      */
     protected final <T extends IObject<?>> T generate( final String p_file, final EObjectFactory p_factory, final Object... p_arguments )
     {
+        Assume.assumeNotNull( m_actions );
         Assume.assumeNotNull( m_environment );
 
         try
@@ -81,7 +99,7 @@ public abstract class IBaseTest
         )
         {
 
-            return p_factory.generate( l_stream, ACTIONS.stream(), IAggregation.EMPTY, m_environment ).generatesingle( p_arguments ).raw();
+            return p_factory.generate( l_stream, m_actions.stream(), IAggregation.EMPTY, m_environment ).generatesingle( p_arguments ).raw();
 
         }
         catch ( final Exception l_exception )
@@ -105,6 +123,7 @@ public abstract class IBaseTest
      */
     protected final <T extends IObject<?>> List<T> generate( final String p_file, final EObjectFactory p_factory, final int p_number, final Object... p_arguments )
     {
+        Assume.assumeNotNull( m_actions );
         Assume.assumeNotNull( m_environment );
 
         try
@@ -113,7 +132,7 @@ public abstract class IBaseTest
         )
         {
 
-            return p_factory.generate( l_stream, ACTIONS.stream(), IAggregation.EMPTY, m_environment ).generatemultiple( p_number, p_arguments ).map( IAgent::<T>raw ).collect( Collectors.toList() );
+            return p_factory.generate( l_stream, m_actions.stream(), IAggregation.EMPTY, m_environment ).generatemultiple( p_number, p_arguments ).map( IAgent::<T>raw ).collect( Collectors.toList() );
 
         }
         catch ( final Exception l_exception )
