da0f896f7d24bf69ce42ee96aef080255022317e
Philipp Kraus <philipp.kraus@flashpixx.de>
2017-05-06 09:52:24 +0200

    add license

--- src/main/java/org/lightjason/trafficsimulation/simulation/environment/IBaseEnvironmentGenerator.java
+++ src/main/java/org/lightjason/trafficsimulation/simulation/environment/IBaseEnvironmentGenerator.java
@@ -1,11 +1,34 @@
+/*
+ * @cond LICENSE
+ * ######################################################################################
+ * # LGPL License                                                                       #
+ * #                                                                                    #
+ * # This file is part of the LightJason TrafficSimulation                              #
+ * # Copyright (c) 2016-17, LightJason (info@lightjason.org)                            #
+ * # This program is free software: you can redistribute it and/or modify               #
+ * # it under the terms of the GNU Lesser General Public License as                     #
+ * # published by the Free Software Foundation, either version 3 of the                 #
+ * # License, or (at your option) any later version.                                    #
+ * #                                                                                    #
+ * # This program is distributed in the hope that it will be useful,                    #
+ * # but WITHOUT ANY WARRANTY; without even the implied warranty of                     #
+ * # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                      #
+ * # GNU Lesser General Public License for more details.                                #
+ * #                                                                                    #
+ * # You should have received a copy of the GNU Lesser General Public License           #
+ * # along with this program. If not, see http://www.gnu.org/licenses/                  #
+ * ######################################################################################
+ * @endcond
+ */
+
 package org.lightjason.trafficsimulation.simulation.environment;
 
 import org.lightjason.agentspeak.action.IAction;
 import org.lightjason.agentspeak.configuration.IAgentConfiguration;
 import org.lightjason.agentspeak.generator.IBaseAgentGenerator;
 import org.lightjason.agentspeak.language.score.IAggregation;
-import org.lightjason.trafficsimulation.ui.CHTTPServer;
 import org.lightjason.trafficsimulation.simulation.algorithm.routing.IRouting;
+import org.lightjason.trafficsimulation.ui.CHTTPServer;
 
 import java.io.InputStream;
 import java.util.stream.Collectors;
