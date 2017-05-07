18c977f53c3d9f5d60a86c9c4c7265d49de449d0
Philipp Kraus <philipp.kraus@flashpixx.de>
2017-05-07 14:42:40 +0200

    add beliefbase for configuration

--- src/main/java/org/lightjason/trafficsimulation/CConfiguration.java
+++ src/main/java/org/lightjason/trafficsimulation/CConfiguration.java
@@ -23,6 +23,15 @@
 
 package org.lightjason.trafficsimulation;
 
+import org.lightjason.agentspeak.beliefbase.CEmptyBeliefbase;
+import org.lightjason.agentspeak.beliefbase.IBeliefbase;
+import org.lightjason.agentspeak.beliefbase.view.IView;
+import org.lightjason.agentspeak.beliefbase.view.IViewGenerator;
+import org.lightjason.agentspeak.common.CPath;
+import org.lightjason.agentspeak.common.IPath;
+import org.lightjason.agentspeak.language.ILiteral;
+import org.lightjason.agentspeak.language.instantiable.plan.trigger.ITrigger;
+import org.lightjason.trafficsimulation.simulation.environment.IEnvironment;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.File;
@@ -36,7 +45,8 @@ import java.nio.file.StandardCopyOption;
 import java.util.Arrays;
 import java.util.Locale;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.Objects;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -55,7 +65,7 @@ public final class CConfiguration
     /**
      * map with configuration data
      */
-    private final Map<String, Object> m_configuration = new ConcurrentHashMap<>();
+    private final Map<String, Object> m_configuration = new TreeMap<>( String.CASE_INSENSITIVE_ORDER );
 
 
     /**
@@ -65,6 +75,7 @@ public final class CConfiguration
     private CConfiguration()
     {
     }
+
 
     /**
      * loads the configuration
@@ -76,24 +87,17 @@ public final class CConfiguration
     {
 
         try
-            (
+        (
                 final InputStream l_stream = orDefaultPath( p_path );
-            )
+        )
         {
-            final Map<String, ?> l_result = (Map<String, Object>) new Yaml().load( l_stream );
-            if ( l_result != null )
-            {
-                m_configuration.clear();
-                m_configuration.putAll( l_result );
-            }
-
+            return this.set( new Yaml().load( l_stream ) );
         } catch ( final IOException l_exception )
         {
             throw new RuntimeException( l_exception );
         }
-
-        return this;
     }
+
 
     /**
      * loads the configuration from a string
@@ -104,7 +108,20 @@ public final class CConfiguration
     @SuppressWarnings( "unchecked" )
     public final CConfiguration loadstring( final String p_yaml )
     {
-        final Map<String, ?> l_result = (Map<String, Object>) new Yaml().load( p_yaml );
+        return this.set( new Yaml().load( p_yaml ) );
+    }
+
+
+    /**
+     * push data to the internal map
+     *
+     * @param p_data input data
+     * @return self reference
+     */
+    @SuppressWarnings( "unchecked" )
+    private CConfiguration set( final Object p_data )
+    {
+        final Map<String, ?> l_result = (Map<String, Object>) maptolowercase( p_data );
         if ( l_result != null )
         {
             m_configuration.clear();
@@ -112,6 +129,51 @@ public final class CConfiguration
         }
         return this;
     }
+
+
+    /**
+     * convert all map keys to lower-case
+     *
+     * @param p_value any value
+     * @return object return
+     */
+    @SuppressWarnings( "unchecked" )
+    private static Object maptolowercase( final Object p_value )
+    {
+        if ( !( p_value instanceof Map<?, ?> ) )
+            return p_value;
+
+        final Map<String, Object> l_map = new TreeMap<>( String.CASE_INSENSITIVE_ORDER );
+        l_map.putAll(
+                       ( (Map<String, Object>) p_value ).entrySet()
+                                                       .parallelStream()
+                                                       .collect( Collectors.toMap(
+                                                           i -> i.getKey().toLowerCase(),
+                                                           i -> maptolowercase( i.getValue() )
+                                                       ) )
+        );
+        return l_map;
+    }
+
+
+    @Override
+    public final int hashCode()
+    {
+        return 1;
+    }
+
+    @Override
+    public boolean equals( final Object p_object )
+    {
+        return ( p_object != null ) && ( p_object instanceof CConfiguration ) && ( p_object.hashCode() == this.hashCode() );
+    }
+
+    @Override
+    public final String toString()
+    {
+        return m_configuration.toString();
+    }
+
 
     /**
      * set default path
@@ -125,6 +187,7 @@ public final class CConfiguration
                ? CConfiguration.class.getResourceAsStream( "configuration.yaml" )
                : new FileInputStream( p_path );
     }
+
 
     /**
      * creates the default configuration
@@ -149,6 +212,7 @@ public final class CConfiguration
         return l_path;
     }
 
+
     /**
      * returns a configuration value
      *
@@ -160,6 +224,7 @@ public final class CConfiguration
     {
         return recursivedescent( m_configuration, p_path );
     }
+
 
     /**
      * returns a configuration value or on not
@@ -198,4 +263,203 @@ public final class CConfiguration
                ? (T) l_data
                : (T) recursivedescent( (Map<String, ?>) l_data, Arrays.copyOfRange( p_path, 1, p_path.length ) );
     }
+
+
+
+    /**
+     * view of the configuration
+     */
+    private final class CView implements IView<IEnvironment>
+    {
+        /**
+         * name of the view
+         */
+        private static final String NAME = "config";
+        /**
+         * parent name
+         */
+        private final IView<IEnvironment> m_parent;
+
+        /**
+         * ctor
+         *
+         * @param p_parent parent view
+         */
+        CView( final IView<IEnvironment> p_parent )
+        {
+            m_parent = p_parent;
+        }
+
+        @Override
+        public final int hashCode()
+        {
+            return NAME.hashCode();
+        }
+
+        @Override
+        public final boolean equals( final Object p_object )
+        {
+            return ( p_object != null ) && ( p_object instanceof IView<?> ) && ( p_object.hashCode() == this.hashCode() );
+        }
+
+        @Override
+        @SafeVarargs
+        public final Stream<IView<IEnvironment>> walk( final IPath p_path, final IViewGenerator<IEnvironment>... p_generator )
+        {
+            return Stream.of( this );
+        }
+
+        @Override
+        public final IView<IEnvironment> generate( final IViewGenerator<IEnvironment> p_generator, final IPath... p_paths )
+        {
+            return this;
+        }
+
+        @Override
+        public final Stream<IView<IEnvironment>> root()
+        {
+            return Stream.concat(
+                Stream.of( this ),
+                Stream.of( this.parent() ).filter( Objects::nonNull )
+            );
+        }
+
+        @Override
+        public final IBeliefbase<IEnvironment> beliefbase()
+        {
+            return CEmptyBeliefbase.instance();
+        }
+
+        @Override
+        public final IPath path()
+        {
+            final IPath l_path = new CPath();
+            this.root().forEach( i -> l_path.pushfront( i.name() ) );
+            return l_path;
+        }
+
+        @Override
+        public final String name()
+        {
+            return NAME;
+        }
+
+        @Override
+        public final IView<IEnvironment> parent()
+        {
+            return m_parent;
+        }
+
+        @Override
+        public final boolean hasParent()
+        {
+            return m_parent != null;
+        }
+
+        @Override
+        public final Stream<ITrigger> trigger()
+        {
+            return Stream.of();
+        }
+
+        @Override
+        public final Stream<ILiteral> stream( final IPath... p_path )
+        {
+
+            return Stream.of();
+        }
+
+        @Override
+        public final Stream<ILiteral> stream( final boolean p_negated, final IPath... p_path )
+        {
+            return Stream.of();
+        }
+
+        @Override
+        public final IView<IEnvironment> clear( final IPath... p_path )
+        {
+            return this;
+        }
+
+        @Override
+        public final IView<IEnvironment> add( final Stream<ILiteral> p_literal )
+        {
+            return this;
+        }
+
+        @Override
+        public final IView<IEnvironment> add( final ILiteral... p_literal )
+        {
+            return this;
+        }
+
+        @Override
+        @SafeVarargs
+        public final IView<IEnvironment> add( final IView<IEnvironment>... p_view )
+        {
+            return this;
+        }
+
+        @Override
+        @SafeVarargs
+        public final IView<IEnvironment> add( final IPath p_path, final IView<IEnvironment>... p_view )
+        {
+            return this;
+        }
+
+        @Override
+        public final IView<IEnvironment> remove( final Stream<ILiteral> p_literal )
+        {
+            return this;
+        }
+
+        @Override
+        public final IView<IEnvironment> remove( final ILiteral... p_literal )
+        {
+            return this;
+        }
+
+        @Override
+        public final IView<IEnvironment> remove( final IView<IEnvironment> p_view
+        )
+        {
+            return this;
+        }
+
+        @Override
+        public final boolean containsLiteral( final IPath p_path )
+        {
+            final Object l_value = recursivedescent( m_configuration, p_path.stream().toArray( String[]::new ) );
+            return ( l_value != null ) && ( !( l_value instanceof Map<?, ?> ) );
+        }
+
+        @Override
+        public final boolean containsView( final IPath p_path )
+        {
+            final Object l_value = recursivedescent( m_configuration, p_path.stream().toArray( String[]::new ) );
+            return ( l_value != null ) && ( l_value instanceof Map<?, ?> );
+        }
+
+        @Override
+        public final boolean empty()
+        {
+            return m_configuration.isEmpty();
+        }
+
+        @Override
+        public final int size()
+        {
+            return 0;
+        }
+
+
+
+        @Override
+        public final IEnvironment update( final IEnvironment p_agent )
+        {
+            return p_agent;
+        }
+
+    }
+
 }
